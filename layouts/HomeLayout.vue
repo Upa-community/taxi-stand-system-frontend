2fd9df880e2611826dd3570b7043e2fd04e7be3e
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-03-11 01:44:16 +0900

    コンポーネントが動作しない不具合を修正

--- layouts/HomeLayout.vue
+++ layouts/HomeLayout.vue
@@ -1,6 +1,30 @@
 <template>
     <v-app dark>
-        <SideNavigation />
+        <v-navigation-drawer
+            v-model="drawer"
+            :mini-variant="miniVariant"
+            :clipped="clipped"
+            fixed
+            app
+            class="navigation"
+        >
+            <v-list>
+                <v-list-item
+                    v-for="(item, i) in items"
+                    :key="i"
+                    :to="item.to"
+                    router
+                    exact
+                >
+                    <v-list-item-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
         <v-app-bar :clipped-left="clipped" fixed app>
             <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
             <v-btn icon @click.stop="miniVariant = !miniVariant">
@@ -40,18 +64,25 @@
 </template>
 
 <script>
-import SideNavigation from "~/components/Home/SideNavigation.vue";
-
 export default {
     name: 'HomeLayout',
-    components: {
-        SideNavigation
-    },
     data() {
         return {
             clipped: false,
             drawer: false,
             fixed: false,
+            items: [
+                {
+                    icon: 'mdi-apps',
+                    title: 'Welcome',
+                    to: '/',
+                },
+                {
+                    icon: 'mdi-chart-bubble',
+                    title: 'Inspire',
+                    to: '/inspire',
+                },
+            ],
             miniVariant: false,
             right: true,
             rightDrawer: false,
