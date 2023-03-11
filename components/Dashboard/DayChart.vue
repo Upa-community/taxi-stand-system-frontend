c40d1d5673d309cc40332c21b6c648453376d6c2
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-03-11 17:29:41 +0900

    Day chart用のコンポーネントを追加

---
+++ components/Dashboard/DayChart.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-card
+        class="mx-auto"
+    > 
+        <v-card-item>
+            <v-card-title class="headline">Day chart</v-card-title>
+            <div class="size">
+                <DayChartData :styles="chartStyles" /> 
+            </div>               
+        </v-card-item>
+    </v-card>            
+</template>
+  
+<script>
+import DayChartData from '~/components/Dashboard/DayChartData.vue'
+  
+export default {
+    components: {
+        DayChartData
+    },
+    data() {
+        return{
+            height: 100
+        }
+    },
+    computed:{
+        chartStyles() {
+            return{
+                height: `${this.height}%`,
+                position:'relative'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.size{
+    height: 320px;
+}
+</style>
--- pages/home.vue
+++ pages/home.vue
@@ -1,16 +1,29 @@
 <template>
-    <MonthChart class="mx-auto" />
+    <v-container>
+        <v-row>
+            <v-col col="12">
+                <MonthChart class="mx-auto" />
+            </v-col>      
+        </v-row>    
+        <v-row>
+            <v-col col="12">
+                <DayChart class="mx-auto" />
+            </v-col>  
+        </v-row>    
+    </v-container>
 </template>
 
 <script>
 import MonthChart from "~/components/Dashboard/MonthChart.vue";
+import DayChart from "~/components/Dashboard/DayChart.vue";
 
 export default {
     name: "IndexPage",
     layout: "HomeLayout",
     middleware: "auth",
     components: {
-        MonthChart
+        MonthChart,
+        DayChart
     }
 }
 </script>
