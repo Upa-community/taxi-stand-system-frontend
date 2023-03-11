5cac3d24fce40f5328177f7bbe0432336c9ed828
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-03-11 23:58:26 +0900

    Google Mapのプラグインを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vue2-google-maps.js' }
+    {
+      src: '~/plugins/vue2-google-maps.js'
+    }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
