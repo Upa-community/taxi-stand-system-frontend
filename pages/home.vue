c1292cd95e77ab52d855301b30c9b077d05e06aa
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-03-12 01:52:15 +0900

    Google Mapのテンプレートを追加

--- pages/home.vue
+++ pages/home.vue
@@ -1,16 +1,16 @@
 <template>
     <v-container>
         <v-row>
-            <v-col :cols="12">
+            <v-col :cols="10">
                 <MonthChart class="mx-auto" />
-            </v-col>      
-        </v-row>    
-        <v-row>
+            </v-col>
             <v-col :cols="2">
                 <CongestionChart class="mx-auto" />
-            </v-col> 
-            <v-col :cols="4">
-                <DayChart class="mx-auto" />
+            </v-col>  
+        </v-row>    
+        <v-row>
+            <v-col :cols="6">
+                <SpotsMap class="mx-auto" />
             </v-col> 
             <v-col :cols="6">
                 <DayChart class="mx-auto" />
@@ -23,6 +23,7 @@
 import MonthChart from "~/components/Dashboard/MonthChart.vue";
 import CongestionChart from "~/components/Dashboard/CongestionChart.vue";
 import DayChart from "~/components/Dashboard/DayChart.vue";
+import SpotsMap from "~/components/Dashboard/SpotsMap.vue";
 
 export default {
     name: "IndexPage",
@@ -31,7 +32,8 @@ export default {
     components: {
         MonthChart,
         CongestionChart,
-        DayChart
+        DayChart,
+        SpotsMap
     }
 }
 </script>
