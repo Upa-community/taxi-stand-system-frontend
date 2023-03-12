33dfc465214844ec7215274bff6441faafc86804
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-03-12 22:13:40 +0900

    カードの表記を変更

--- components/Dashboard/DayChart.vue
+++ components/Dashboard/DayChart.vue
@@ -5,7 +5,7 @@
         style="background: linear-gradient(#14F3FF, #6E00B2);"
     > 
         <v-card-item>
-            <v-card-title class="headline">Day chart</v-card-title>
+            <v-card-title class="headline">Day</v-card-title>
             <div style="height: 350px;">
                 <DayChartData :styles="chartStyles" /> 
             </div>               
