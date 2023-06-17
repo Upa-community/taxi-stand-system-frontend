b352f45473dde727119910a210ac3a41946ad5a3
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-06-18 02:05:19 +0900

    地点マップにマーカーを追加

--- components/Dashboard/SpotsMap.vue
+++ components/Dashboard/SpotsMap.vue
@@ -5,15 +5,45 @@
         style="background: #ffffff;"
     > 
         <v-card-item>
-            <v-card-title class="headline">駐輪場マップ</v-card-title>
+            <v-card-title class="headline">地点マップ</v-card-title>
             <div style="height: 350px;">
                 <GmapMap
+                    ref="gmp"
                     map-type-id="roadmap"
                     :center="maplocation"
                     :zoom="zoom"
                     :style="styleMap"
                     :options="mapOptions"
                 >
+                    <GmapMarker
+                        v-for="(m, index) in markers"
+                        :key="index"
+                        :title="m.title"
+                        :position="m.position"
+                        :clickable="true"
+                        :draggable="false"
+                        @click="onClickMarker(m)"
+                    />
+                    <GmapInfoWindow
+                        :options="infoOptions"
+                        :position="infoWindowPos"
+                        :opened="infoWinOpen"
+                        @closeclick="infoWinOpen = false"
+                    >
+                        <p style="color: #000">
+                        {{ marker.title }}
+                        </p>
+                        <iframe
+                            width="400px"
+                            height="350px"
+                            :src="marker.url"
+                            title="YouTube video player" 
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen
+                        >
+                        </iframe>
+                    </GmapInfoWindow>
                 </GmapMap>
             </div>               
         </v-card-item>
@@ -25,6 +55,7 @@ export default {
     data() {
         return{
             height: 100,
+            zoom: 9,
             maplocation: {
                 lat: 35.6581,
                 lng: 139.7017 
@@ -33,7 +64,6 @@ export default {
                 width: '100%',
                 height: '100%'
             },
-            zoom: 9,
             mapOptions: {
                 zoomControl: false,
                 fullscreenControl: false,
@@ -178,8 +208,56 @@ export default {
                         ]
                     }
                 ]
+            },
+            infoOptions: {
+                minWidth: 400,
+                minHeight: 350,
+                pixelOffset: {
+                    width: 0,
+                    height: 0,
+                },
+            },
+            infoWindowPos: null,
+            infoWinOpen: false,
+            marker: {},
+            markers: []
+        }
+    },
+    computed: {
+        getHomeData: function() {
+            return this.$store.getters["homeData/getHomeData"];
+        }
+    },
+    watch: {
+        getHomeData(values) {
+            var spots = values.spots_data;
+
+            for (let i = 0; i < spots.length; i++) {
+                var urlReplace = spots[i].spots_url.replace('watch?v=', 'embed/');
+                const dataset = {
+                    title: spots[i].spots_name,
+                    url: urlReplace,
+                    position: {
+                        lat: parseFloat(spots[i].spots_latitude),
+                        lng: parseFloat(spots[i].spots_longitude) 
+                    }
+                }
+                this.markers.push(dataset);
             }
         }
+    },
+    methods: {
+    getCurrentPosition() {
+      return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+      })
+    },
+    onClickMarker(marker) {
+      this.$refs.gmp.panTo(marker.position)
+      this.infoWindowPos = marker.position
+      this.marker = marker
+      this.infoWinOpen = true
     }
+  }
 }
 </script>
